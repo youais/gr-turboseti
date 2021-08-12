d50d886e9eebe8609d91d385dcabb7817e1a8b54
Yiwei Chai <mychai@sas.upenn.edu>
2021-08-11 17:47:56 -0700

    fixed multiprocessing_test.py

--- multiprocessing_test.py
+++ multiprocessing_test.py
@@ -5,7 +5,7 @@ import time
 """
 Issues:
     - Will output hi_square() if spectra.shape[0] >= 5 without the while loop
-    - Once the while loop is added, it won't log anything after line 36 
+    - Once the while loop is added, it won't log anything after line 36
 
 """
 
@@ -18,6 +18,29 @@ def hi_square(spectra, num):
     print(num, "squared:", num*num)
 
 i = 0
+
+if __name__ == "__main__":
+    with mp.Pool(processes=1) as pool:
+        while True:
+            if spectra.shape[0] < 5: # spectra rows < 60
+                print("Buffer spectrum row #:", i, "/5")
+                print("input_items[0]", input_items[i])
+                print("input_items[0] shape:", input_items[i].shape)
+                print("spectra shape:", spectra.shape)
+                spectra = np.append(spectra, input_items[i], axis=0)
+                print("Done.")
+                print("New spectra shape:", spectra.shape)
+                i += 1
+                print("Upcoming row #:", i, "/60")
+            else:
+                print("Spectra:", spectra)
+                print("Spectra shape:", spectra.shape)
+                result = pool.apply_async(hi_square, (spectra, 10)) # evaluates hi_square(spectra, 10) asynchronously in a single process
+                print(result.get())
+
+
+'''
+Old Version, fixed with help from Zach Yek.
 
 while True:
     if spectra.shape[0] < 5: # spectra rows < 60
@@ -37,3 +60,4 @@ while True:
             with mp.Pool(processes=1) as pool:         # start 1 worker processes
                 result = pool.apply_async(hi_square, (spectra, 10)) # evaluates hi_square(spectra, 10) asynchronously in a single process
                 print(result.get()) # should print "Hello world!", "Spectra shape: (5, 1000000)", and "10 squared: 100"
+'''
